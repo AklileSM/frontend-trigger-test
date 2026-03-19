07170b5f79c689a10314b99e9d9c1d752450b747
AklileSM <atm9561@nyu.edu>
2026-03-19 16:34:15 +0400

    please this time

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>Another CD change</h1>
+          <h1>And AGAIN?</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
